361525f77419d8fdd1edcb93f591f04379a246f9
GeorgeSemenov <zhora.sem@yandex.ru>
2016-07-25 17:10:55 +0300

    Now dropdown menu in dropdown menu is complete!

--- js/hiding-menu.js
+++ js/hiding-menu.js
@@ -1,14 +1,34 @@
-var clicked = false;
+var is_clicked_hidden_menu = false;
+var is_open_droppdown_menu = false;
 $(document).ready(function(){
     var hm = $('.hidden-menu');
+    var him = $('.hidden-menu>li>ul');
+    var his = $('.hidden-menu>li>span');
     $(".fa-bars").click(function(){
-        if (clicked == false) {
-            clicked = true;
+        if (is_clicked_hidden_menu == false) {
+            is_clicked_hidden_menu = true;
             hm.css({display:"inherit", opacity: "1"});
-            $('#hiding_menu_inner').css({display: "inherit", height: "auto"});
         } else {
-            clicked = false;
+            is_clicked_hidden_menu = false;
             hm.css({display:"none",opacity:"0"});
-            $('#hiding_menu_inner').css("height", "0px");
+            his.css('text-decoration','none');
+            him.css('display','none');
     }})
+    $(".hidden-menu>li>span").click(function(){
+        if (is_open_droppdown_menu == false){
+            is_open_droppdown_menu = true;
+            $(this).css({textDecoration:"underline"});
+            $(this).siblings().css("display","inherit");
+        } else if ((is_clicked_hidden_menu == true) && !($(this).css('textDecoration')=="underline")){
+            //alert('miscelenious');
+            his.css('text-decoration','none');
+            him.css('display','none');
+            $(this).css('text-decoration','underline');
+            $(this).siblings().css('display', 'inherit');
+        }else{
+            is_open_droppdown_menu = false;
+            $(this).css({textDecoration:"none"});
+            $(this).siblings().css('display','none');
+        }
+    })
 });
